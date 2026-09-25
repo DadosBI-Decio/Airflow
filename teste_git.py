7d71d9030d09d622a061bf1143ed36625bf911d8
Felipe Souza <felipe.souza@grupodecio.com.br>
2026-09-25 13:42:19 -0300

    Adicionando teste

--- teste_git.py
+++ teste_git.py
@@ -1,1 +1,30 @@
-print("Teste")
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+with DAG(
+    dag_id="teste_scheduler",
+    start_date=datetime(2026, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["teste"],
+) as dag:
+
+    tarefa_1 = BashOperator(
+        task_id="tarefa_1",
+        bash_command='echo "Executando tarefa 1"',
+    )
+
+    tarefa_2 = BashOperator(
+        task_id="tarefa_2",
+        bash_command='echo "Executando tarefa 2"',
+    )
+
+    tarefa_3 = BashOperator(
+        task_id="tarefa_3",
+        bash_command='echo "Executando tarefa 3"',
+    )
+
+    tarefa_1 >> tarefa_2 >> tarefa_3
